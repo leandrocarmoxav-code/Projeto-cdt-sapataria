4ad9e9f023bc44e1f988f74cfc86fcf36153b27f
Mariadudaemvocacao <leandrocarmoxav@gmail.com>
2026-05-26 15:12:27 -0300

    update projeto

--- api/app.py
+++ api/app.py
@@ -2,40 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Seus dados do estoque
 estoque = [
-
     {
         "marca": "Nike",
         "modelo": "Air Max 90",
-        "preco": 799.90
+        "preco": 799.90,
     },
-
     {
         "marca": "Adidas",
         "modelo": "Ultraboost",
-        "preco": 699.90
+        "preco": 699.90,
     },
-
     {
         "marca": "Jordan",
         "modelo": "Jordan 4 Retro",
-        "preco": 1799.90
+        "preco": 1799.90,
     }
-
 ]
 
 @app.route("/")
 def inicio():
-
-    return render_template(
-        "index.html",
-        estoque=estoque
-    )
-
-@app.route("/teste")
-def teste():
-
-    return "Flask funcionando!"
-
-# IMPORTANTE PARA O VERCEL
-app = app
+    return render_template("index.html", estoque=estoque)
